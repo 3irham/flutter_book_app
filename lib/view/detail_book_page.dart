94086195fa276d388481ebfe2d72da10768dd92f
3irham <ikmuttaqin@gmail.com>
2022-07-18 14:59:35 +0700

    Part 3

--- lib/view/detail_book_page.dart
+++ lib/view/detail_book_page.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class BookListPage extends StatefulWidget {
-  const BookListPage({super.key});
+class DetailBookPage extends StatefulWidget {
+  const DetailBookPage({super.key});
 
   @override
-  State<BookListPage> createState() => _BookListPageState();
+  State<DetailBookPage> createState() => _DetailBookPageState();
 }
 
-class _BookListPageState extends State<BookListPage> {
-  fetchBookApi() async {
-    var url = Uri.parse('https://api.itbook.store/1.0/new');
+class _DetailBookPageState extends State<DetailBookPage> {
+  fetchDetailBookApi() async {
+    var url = Uri.parse('https://api.itbook.store/1.0/books/9781484206485');
     var response =
         await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
     print('Response status: ${response.statusCode}');
@@ -20,17 +20,10 @@ class _BookListPageState extends State<BookListPage> {
   }
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    fetchBookApi();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Book Catalogue'),
+        title: const Text('Detail'),
         centerTitle: true,
       ),
       body: Container(),
